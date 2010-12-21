6df0322b68ebe1ea6707a80e1adaee9cfae5af43
Matthew Boston <matthew.boston@gmail.com>
2010-12-21 17:22:33 -0500

    fixed test-path in project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[speclj "1.1.0"]]
-  :test-path "sepc/")
+  :test-path "spec/")
