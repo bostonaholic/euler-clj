32edd20283635b472ebe5db06c0b6db176be2e05
Matthew Boston <matthew.boston@gmail.com>
2011-01-06 14:48:37 -0500

    updating speclj to 1.2.0

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "bostonaholic's Project Euler solutions"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[speclj "1.1.0"]]
+  :dev-dependencies [[speclj "1.2.0"]]
   :test-path "spec/")
