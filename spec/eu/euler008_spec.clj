038cdad30758b3861aa1a55520803a3b8757a9ad
bostonaholic <matthew.boston@gmail.com>
2011-01-20 19:28:14 -0500

    Changed method name to be more descriptive.

--- spec/eu/euler008_spec.clj
+++ spec/eu/euler008_spec.clj
@@ -14,9 +14,9 @@
     (should= '((2 5) (5 6)) (n-consecutive-digits 2 256)))
   
   (it "should multiply the inner collections"
-    (should= '(2 5) (product-of-inner-collection '((2) (5))))
-    (should= '(10) (product-of-inner-collection '((2 5))))
-    (should= '(10 30) (product-of-inner-collection '((2 5) (5 6)))))
+    (should= '(2 5) (product-of-inner-collections '((2) (5))))
+    (should= '(10) (product-of-inner-collections '((2 5))))
+    (should= '(10 30) (product-of-inner-collections '((2 5) (5 6)))))
 
   (it "should find the greatest product of n consecutive digits"
     (should= 5 (euler008 1 25))
