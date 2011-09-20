04f0bfaf41d4139400350e0c59bbc462b7154eb4
Matthew Boston <matthew.boston@gmail.com>
2011-09-20 08:46:14 -0500

    extracting functions in palindrome algorithm

--- spec/eu/core_spec.clj
+++ spec/eu/core_spec.clj
@@ -4,6 +4,16 @@
 
 (describe "Project Euler eu.core tests"
 
+  (it "should detect very short strings"
+    (should (very-short-string? "1"))
+    (should (very-short-string? "")))
+
+  (it "should detect string of greater than length 3 to not be short"
+    (should-not (very-short-string? "123")))
+
+  (it "should detect when the first and last characters are equal"
+    (should (first-and-last-equal? "11")))
+
   (it "should recognize a palindrome"
     (should (palindrome? 1))
     (should-not (palindrome? 12))
--- src/eu/core.clj
+++ src/eu/core.clj
@@ -1,12 +1,18 @@
 (ns eu.core)
 
+(defn very-short-string? [s]
+  (or (= 0 (.length s))
+      (= 1 (.length s))))
+
+(defn first-and-last-equal? [s]
+  ;((= first last) (str s)))
+  (= (first (str s))
+     (last (str s))))
+
 (defn palindrome? [s]
   (cond
-    (or (= 0 (.length (str s)))
-        (= 1 (.length (str s))))
-      true
-    (= (first (str s))
-       (last (str s)))
+    (very-short-string? (str s)) true
+    (first-and-last-equal? s)
       (recur (subs (str s)
                    1
                    (- (.length (str s)) 1)))
