6dd33219dad1d57d5ae99c2169ff462d7964c4c2
Matthew Boston <matthew.boston@gmail.com>
2011-09-20 21:28:51 -0500

    use middle function from bostonaholic.utils.string

--- src/eu/core.clj
+++ src/eu/core.clj
@@ -1,21 +1,17 @@
-(ns eu.core)
+(ns eu.core
+  (:use [bostonaholic.utils.string :only (middle)]))
 
 (defn very-short-string? [s]
   (or (= 0 (.length s))
       (= 1 (.length s))))
 
 (defn first-and-last-equal? [s]
-  ;((= first last) (str s)))
   (= (first (str s))
      (last (str s))))
 
 (defn palindrome? [s]
   (cond
     (very-short-string? (str s)) true
-    (first-and-last-equal? s)
-      (recur (subs (str s)
-                   1
-                   (- (.length (str s)) 1)))
-    :else
-      false))
+    (first-and-last-equal? s)    (recur (middle s))
+    :else                        false))
 
