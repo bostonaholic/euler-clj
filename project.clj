36e0956886d5ab3507fc2a9ab8f25cd8bd7397cc
Matthew Boston <matthew.boston@gmail.com>
2011-09-20 21:28:08 -0500

    bostonaholic.utils 0.3.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject eu "1.0.0-SNAPSHOT"
   :description "bostonaholic's Project Euler solutions"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [bostonaholic/utils "0.2.0"]]
+                 [bostonaholic/utils "0.3.1"]]
   :dev-dependencies [[speclj "1.4.0"]]
   :test-path "spec")
