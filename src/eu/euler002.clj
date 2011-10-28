acd6ee849af697fdce18c9ae93a4bc570ed04f50
Matthew Boston <matthew.boston@gmail.com>
2011-10-28 00:23:42 -0400

    euler002 refactor

--- src/eu/euler002.clj
+++ src/eu/euler002.clj
@@ -1,6 +1,15 @@
-(ns eu.euler002
-  (:use eu.core
-        bostonaholic.utils.math.fibonacci))
+(ns eu.euler002)
+
+(defn fib-sequence []
+  ((fn fib-recur [a b]
+     (cons a
+           (lazy-seq (fib-recur b (+ a b))))) 0 1))
+
+(defn fib [n]
+  (take n (fib-sequence)))
+
+(defn fib-less-than [x]
+  (take-while #(< % x) (fib-sequence)))
 
 (defn euler002 [x]
   (reduce + (filter even? (fib-less-than x))))
