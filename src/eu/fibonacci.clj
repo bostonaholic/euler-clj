681a48d76bd623ef6936db9e2b2eaff87b4fee32
Matthew Boston <matthew.boston@gmail.com>
2011-10-28 00:35:46 -0400

    add comments to fibonacci functions

--- src/eu/fibonacci.clj
+++ src/eu/fibonacci.clj
@@ -1,12 +1,15 @@
 (ns eu.fibonacci)
 
 (defn fib-sequence []
+  "lazy-seq representing the fibonacci sequence"
   ((fn fib-recur [a b]
      (cons a
            (lazy-seq (fib-recur b (+ a b))))) 0 1))
 
 (defn fib [n]
+  "takes n elements of the fibonacci sequence"
   (take n (fib-sequence)))
 
 (defn fib-less-than [x]
+  "takes the fibonacci sequence up to x"
   (take-while #(< % x) (fib-sequence)))
