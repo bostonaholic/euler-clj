fbedcc22ba9c721ff39621e99d6125e1cdeafa51
Matthew Boston <matthew.boston@gmail.com>
2012-10-26 16:14:14 -0400

    #9 solved

--- spec/eu/euler009_spec.clj
+++ spec/eu/euler009_spec.clj
@@ -4,7 +4,14 @@
 
 (describe "Project Euler problem 9"
 
-  (it "should find the Pythagorean triplet"
-    (should (pyth-triplet? 3 4 5))
-    (should-not (pyth-triplet? 4 3 5))))
+  (it "determines if 3 numbers are a Pythagorean triplet"
+      (should (pyth-triplet? [3 4 5]))
+      (should (pyth-triplet? [5 12 13]))
+      (should-not (pyth-triplet? [4 3 5])))
 
+  (it "generates a list of Pythagorean triplets up to a max"
+      (should= [3 4 5] (first (pyth-triplets 12)))
+      (should= [5 12 13] (second (pyth-triplets 100))))
+
+  (it "finds the Pythagorean triplet for which the sum equals the number"
+      (should= [3 4 5] (first (pyth-triplets-for-max 12)))))
--- src/eu/euler009.clj
+++ src/eu/euler009.clj
@@ -1,15 +1,28 @@
 (ns eu.euler009
   (:use eu.math))
 
-(defn pyth-triplet? [a b c]
+(defn pyth-triplet? [[a b c]]
   (and (< a b c)
-       (= (+ (square a) (square b)) (square c))))
+       (= (+ (square a) (square b))
+          (square c))))
 
-(defn sum-of-pyth-triplet [a b c] 
-  (+ a b c))
+(defn pyth-triplets [max]
+  (filter pyth-triplet?
+          (for [a (range 3 (inc max))
+                b (range a (inc max))
+                c (range b (inc max))
+                :while (<= (+ a b c) max)]
+            [a b c])))
+
+(defn pyth-eq? [triplet n]
+  (let [[a b c] triplet]
+    (= (+ a b c) n)))
+
+(defn pyth-triplets-for-max [n]
+  (filter #(pyth-eq? % n) (pyth-triplets n)))
 
 (defn euler009 [n]
-  (= n (sum-of-pyth-triplet)))
+  (apply * (pyth-triplets-for-max n)))
 
 (defn solution []
   (euler009 1000))
