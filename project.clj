7e4560d41054ff622f75eec761861a732dda9627
Matthew Boston <matthew.boston@gmail.com>
2012-10-26 10:46:30 -0400

    update for lein2

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject eu "1.0.0-SNAPSHOT"
   :description "bostonaholic's Project Euler solutions"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[speclj "1.5.2"]]
-  :test-path "spec")
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [speclj "2.3.1"]]
+  :plugins [[speclj "2.3.1"]]
+  :test-path "spec/")
