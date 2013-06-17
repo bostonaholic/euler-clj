27f4fa43f34d84363a51b243584c54868e7fcd9a
Matthew Boston <matthew.boston@gmail.com>
2013-06-17 00:04:48 -0400

    [WIP] working on 11

--- spec/eu/euler011_spec.clj
+++ spec/eu/euler011_spec.clj
@@ -2,4 +2,12 @@
   (:use speclj.core
         eu.euler011))
 
-(describe "Project Euler problem 11")
+(describe "Project Euler problem 11"
+          (it "returns the adjacent items in nested vectors"
+              (should= '(1 2 3 4 6 7 8 9)
+                       (neighbors [[1 2 3] [4 5 6] [7 8 9]] 2)))
+          ;; (it "returns a list of all of the unique adjacent products"
+          ;;     (should= '(16 392 98 2401)
+          ;;              (adjacent-products 2 [[8 2]
+          ;;                                    [49 49]])))
+          )
